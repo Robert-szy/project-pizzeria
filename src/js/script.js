d6ec4fdf74ff4d60ace4aee849478c0641ea0fe8
Robert Szyszka <robert.szyszka@outlook.com>
2021-02-04 22:50:09 +0100

    Add cart toggle

--- src/js/script.js
+++ src/js/script.js
@@ -318,6 +318,36 @@
     }
   }
 
+  class Cart {
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('newCart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions(){
+      const thisCart = this;
+
+      thisCart.dom.toggleTrigger.addEventListener('click', function(event){
+        event.preventDefault();
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+
+    }
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -331,15 +361,21 @@
 
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.Cart = new Cart(cartElem);
+    },
     init: function(){
       const thisApp = this;
 
       thisApp.initData();
       thisApp.initMenu();
-    }
+      thisApp.initCart();
+    },
+
   };
-
-
 
   app.init();
 }
